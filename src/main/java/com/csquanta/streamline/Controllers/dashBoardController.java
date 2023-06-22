7286aa1b1fe6b429208e31c629554c686c89b391
Jubair Ahmed <jubairahmed13260@gmail.com>
2023-06-22 14:52:43 +0600

    login page to dashboard functionality

---
+++ src/main/java/com/csquanta/streamline/Controllers/dashBoardController.java
@@ -0,0 +1,24 @@
+package com.csquanta.streamline.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class dashBoardController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+    @FXML
+    private  Label Userlabel;
+
+    @FXML
+    public void getUser(String user){
+        Userlabel.setText(user);
+    }
+
+
+}
