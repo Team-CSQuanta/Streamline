92fd125449567088788645d046657aff20dcdfa7
Foyez Naeem <86563893+foyeznaeem@users.noreply.github.com>
2023-06-26 09:51:50 +0600

    Updated DashBoard

--- src/main/java/com/csquanta/streamline/Controllers/DashBoardController.java
+++ src/main/java/com/csquanta/streamline/Controllers/DashBoardController.java
@@ -2,6 +2,8 @@ package com.csquanta.streamline.Controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
@@ -11,14 +13,36 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class DashBoardController implements Initializable {
+    @FXML
+    private BarChart<Number, String> todaysTopActivityChart;
 
     @FXML
     private Circle image;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        Image imageSrc = new Image("/Images/Avatar5.png");
-        ImagePattern imagePattern = new ImagePattern(imageSrc);
-        image.setFill(imagePattern);
+//        Image imageSrc = new Image("/Images/Avatar5.png");
+//        ImagePattern imagePattern = new ImagePattern(imageSrc);
+//        image.setFill(imagePattern);
+
+        /**********************************************/
+
+        XYChart.Data<Number, String> playingData  = new XYChart.Data<>(4, "Playing");
+        XYChart.Series<Number, String> playing= new XYChart.Series<>();
+        playing.setName("Playing");
+        playing.getData().add(playingData);
+
+        XYChart.Data<Number, String> readingData = new XYChart.Data<>(13, "Reading");
+        XYChart.Series<Number, String> reading = new XYChart.Series<>();
+        reading.setName("Reading");
+        reading.getData().add(readingData);
+
+        XYChart.Data<Number, String> entertainmentData = new XYChart.Data<>(3, "Entertainment");
+        XYChart.Series<Number, String> entertainment = new XYChart.Series<>();
+        reading.setName("Entertainment");
+        reading.getData().add(entertainmentData);
+
+        todaysTopActivityChart.getData().addAll(reading, entertainment);
+        todaysTopActivityChart.getData().add(playing);
     }
 
     @FXML
