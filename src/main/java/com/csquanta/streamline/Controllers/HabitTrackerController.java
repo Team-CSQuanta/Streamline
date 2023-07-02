b24f83fe4f5f96c792cf4c25faf4181edf495f60
Foyez Naeem <86563893+foyeznaeem@users.noreply.github.com>
2023-07-02 21:56:23 +0600

    Minor Updates

--- src/main/java/com/csquanta/streamline/Controllers/HabitTrackerController.java
+++ src/main/java/com/csquanta/streamline/Controllers/HabitTrackerController.java
@@ -1,19 +1,14 @@
 package com.csquanta.streamline.Controllers;
 
-import animatefx.animation.FadeIn;
 import com.csquanta.streamline.App;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-import java.util.Objects;
 
-public class TodoListController {
+public class HabitTrackerController {
     @FXML
     void onOverviewClicked(MouseEvent event) throws IOException {
         FXMLScene scene = FXMLScene.load("/Fxml/Dashboard.fxml");
@@ -22,12 +17,13 @@ public class TodoListController {
         Stage primaryStage = App.mainStage;
         primaryStage.setScene(dashboard);
     }
+
     @FXML
-    void onHabitTrackerBtnClicked(MouseEvent event) throws IOException{
-        FXMLScene scene = FXMLScene.load("/Fxml/HabitTracker.fxml");
+    void onTasksBtnClicked(MouseEvent event) throws IOException{
+        FXMLScene scene = FXMLScene.load("/Fxml/TodoList.fxml");
         Parent root =  scene.root;
-        Scene habitTracker = new Scene(root);
+        Scene toDoList = new Scene(root);
         Stage primaryStage = App.mainStage;
-        primaryStage.setScene(habitTracker);
+        primaryStage.setScene(toDoList);
     }
 }
