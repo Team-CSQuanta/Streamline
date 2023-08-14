29504aa28cbab8904864bc67b6dc2291666588db
Foyez Naeem <86563893+foyeznaeem@users.noreply.github.com>
2023-08-14 09:50:41 +0600

    Break clock

--- src/main/java/com/csquanta/streamline/CountDown.java
+++ src/main/java/com/csquanta/streamline/CountDown.java
@@ -1,5 +1,7 @@
 package com.csquanta.streamline;
 
+import com.csquanta.streamline.Controllers.SignOutWarningPageController;
+
 import java.util.Objects;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -42,6 +44,7 @@ public final class CountDown {
         isRunning = false;
         timer.cancel();
         observer.timeIsUp();
+        System.out.println("In time is up!");
     }
 
     public void stop() {
