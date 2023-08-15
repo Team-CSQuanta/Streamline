042cdb85b5499ca7e78e627abfbac6b52b892764
Jubair Ahmed <jubairahmed13260@gmail.com>
2023-08-15 13:43:33 +0600

    timer

--- src/main/java/com/csquanta/streamline/Controllers/TimerController.java
+++ src/main/java/com/csquanta/streamline/Controllers/TimerController.java
@@ -1,88 +1,78 @@
 package com.csquanta.streamline.Controllers;
 
-
-import animatefx.animation.FadeIn;
 import com.csquanta.streamline.CountDown;
-import com.csquanta.streamline.Models.Task;
 import com.csquanta.streamline.PomodoroClock;
 import com.csquanta.streamline.TimeMode;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 import java.io.IOException;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.ResourceBundle;
 
-public class TimerController implements Initializable {
-    @FXML
-    private VBox container;
-    @FXML
-    private Label clockLabel;
-    @FXML
-    private ProgressBar clockProgressBar;
-    @FXML
-    private Button toggleBtn;
-    @FXML
-    private Label totalSession;
-    @FXML
-    private Label sessionCount;
-    @FXML
-    public void toggleBtnClicked() {
-        if (countdown.isRunning())
-            stop();     // stops the counter if it is already running
-        else
-            activate(); // activate the counter if it is not running
-    }
-    public Label getTotalSession() {
-        return totalSession;
-    }
+public class TimerController {
+    @FXML private VBox container;
+    @FXML private Label clockLabel;
+    @FXML private ProgressBar clockProgressBar;
+    @FXML private Button toggleBtn;
+    @FXML private Button pomodoroBtn;
+    @FXML private Button shortBreakBtn;
+    @FXML private Button longBreakBtn;
 
-    public void setTotalSession(String text) {
-        this.totalSession.setText(text);
-    }
-
-    @FXML
-    private Button pomodoroBtn;
-    Task task = new Task();
     private CountDown countdown;
     private PomodoroClock clock;
-    private int currentAutoLoop = 1;
-
-
-
-//    public int currentAutoLoop =0;
     private Map<Button, TimeMode> buttonToMode;
-//    @FXML
-//    public void initialize() {
-//        clock = new PomodoroClock(
-//                this, clockLabel, clockProgressBar, TimeMode.POMODORO);
-//        countdown = new CountDown(TimeMode.POMODORO, clock);
-//        initializeButtonToMode();
-//        System.out.println("Total Session in timer: " + totalSession.getText());
-//    }
 
-    private void initializeButtonToMode() {
-        buttonToMode = new HashMap<>();
-        buttonToMode.put(pomodoroBtn, TimeMode.POMODORO);
+    private VBox tempContainerOne;
+    private VBox tempContainerTwo;
 
+    public VBox getTempContainerOne() {
+        return tempContainerOne;
     }
 
+    public void setTempContainerOne(VBox tempContainerOne) {
+        this.tempContainerOne = tempContainerOne;
+    }
+
+    public VBox getTempContainerTwo() {
+        return tempContainerTwo;
+    }
+
+    public void setTempContainerTwo(VBox tempContainerTwo) {
+        this.tempContainerTwo = tempContainerTwo;
+    }
+
+    @FXML
+    public void initialize() {
+        clock = new PomodoroClock(
+                this, clockLabel, clockProgressBar, TimeMode.POMODORO);
+        countdown = new CountDown(TimeMode.POMODORO, clock);
+//        initializeButtonToMode();
+    }
+//
+//    private void initializeButtonToMode() {
+//        buttonToMode = new HashMap<>();
+//        buttonToMode.put(pomodoroBtn, TimeMode.POMODORO);
+//        buttonToMode.put(shortBreakBtn, TimeMode.BREAK);
+//        buttonToMode.put(longBreakBtn, TimeMode.BREAK);
+//    }
+
+    public void toggleBtnClicked() {
+        if (countdown.isRunning())
+            stop();
+        else
+            activate();
+    }
 
     private void stop() {
         countdown.stop();
-        updateToggleBtn("Resume");
+        updateToggleBtn("RESUME");
     }
 
     private void updateToggleBtn(String text) {
@@ -90,6 +80,8 @@ public class TimerController implements Initializable {
     }
 
     private void activate() {
+        if (countdown.isTimeUp())
+            reset();
         start();
     }
 
@@ -102,27 +94,42 @@ public class TimerController implements Initializable {
         container.getStyleClass().remove("time-is-up-background");
         toggleBtn.getStyleClass().remove("time-is-up-color");
     }
+
     private void start() {
-//        System.out.println("max"+maxLoopsCounts);
         countdown.start();
-        updateToggleBtn("Stop");
+        updateToggleBtn("STOP");
+    }
+
+    public void modeBtnClicked(ActionEvent event) {
+        Button button = (Button) event.getSource();
+        TimeMode mode = buttonToMode.get(button);
+        changeMode(mode);
+        highlightModeButton(button);
+    }
+
+    private void changeMode(TimeMode mode) {
+        countdown.setMode(mode);
+        clock.setMode(mode);
+        removeTimeIsUpStyles();
+        start();
+    }
+
+    private void highlightModeButton(Button button) {
+        removeModeButtonHighlighting();
+        button.getStyleClass().add("highlight-btn");
+    }
+
+    private void removeModeButtonHighlighting() {
+        pomodoroBtn.getStyleClass().remove("highlight-btn");
+        shortBreakBtn.getStyleClass().remove("highlight-btn");
+        longBreakBtn.getStyleClass().remove("highlight-btn");
     }
 
     public void timeIsUp() {
+
         addTimeIsUpStyles();
         playSound();
-
-        if ( currentAutoLoop < Integer.parseInt(totalSession.getText())) {
-            reset();
-            currentAutoLoop++;
-            start();
-           sessionCount.setText(String.valueOf(currentAutoLoop));
-            System.out.println(currentAutoLoop);
-
-        } else {
-            stop();
-            updateToggleBtn("Reset");
-        }
+        updateToggleBtn("RESET");
     }
 
     private void addTimeIsUpStyles() {
@@ -131,19 +138,11 @@ public class TimerController implements Initializable {
     }
 
     private void playSound() {
-//       Media sound = new Media(this.getClass().getResource("/Sounds/sound.wav").toString());
-//      MediaPlayer player = new MediaPlayer(sound);
+        Media sound = new Media(this.getClass().getResource("/Sounds/sound.wav").toString());
+        MediaPlayer player = new MediaPlayer(sound);
 
-//        player.play();
+        player.play();
     }
 
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-//        clock = new PomodoroClock(
-//                this, clockLabel, clockProgressBar, TimeMode.POMODORO);
-//        countdown = new CountDown(TimeMode.POMODORO, clock);
-//        initializeButtonToMode();
-//        System.out.println("Total Session in timer: " + totalSession.getText());
-    }
 }
