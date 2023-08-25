0f5b334521a818599154a0af96b37cc702452b10
Foyez Naeem <86563893+foyeznaeem@users.noreply.github.com>
2023-08-25 12:26:19 +0600

    Deleted shop_info

--- src/main/java/com/csquanta/streamline/Networking/ReadThreadServer.java
+++ src/main/java/com/csquanta/streamline/Networking/ReadThreadServer.java
@@ -17,61 +17,58 @@ public class ReadThreadServer extends Thread {
     @Override
     public void run() {
 
-            while (true) {
-                try{
+        while (true) {
+            try {
 
-                    Message receivedMessage = (Message) networkInfo.getNetworkUtil().read();
-                    String sender = receivedMessage.getFrom();
-                    String receiver = receivedMessage.getTo();
-                    NetworkInformation receiverInfo = clientNetworkInformationMap.get(receiver);
+                Message receivedMessage = (Message) networkInfo.getNetworkUtil().read();
+                String sender = receivedMessage.getFrom();
+                String receiver = receivedMessage.getTo();
+                NetworkInformation receiverInfo = clientNetworkInformationMap.get(receiver);
 
-                        if(receivedMessage.getMessageType() == MessageType.CHALLENGE) {
-                            String pomodoroSession = ((ChallengeMessage)receivedMessage).getChallengeTaskPomodoroSession();
-                            String challengeType = ((ChallengeMessage)receivedMessage).getChallengeType();
-                            String challengeDescription = ((ChallengeMessage)receivedMessage).getChallengeDescription();
-                            String taskTag = ((ChallengeMessage)receivedMessage).getChallengeTaskTag();
-                            String monsterName =((ChallengeMessage)receivedMessage).getMonstersName();
-                            String taskTitle = ((ChallengeMessage)receivedMessage).getTaskTitle();
+                if (receivedMessage.getMessageType() == MessageType.CHALLENGE) {
+                    String pomodoroSession = ((ChallengeMessage) receivedMessage).getChallengeTaskPomodoroSession();
+                    String challengeType = ((ChallengeMessage) receivedMessage).getChallengeType();
+                    String challengeDescription = ((ChallengeMessage) receivedMessage).getChallengeDescription();
+                    String taskTag = ((ChallengeMessage) receivedMessage).getChallengeTaskTag();
+                    String monsterName = ((ChallengeMessage) receivedMessage).getMonstersName();
+                    String taskTitle = ((ChallengeMessage) receivedMessage).getTaskTitle();
 
 
-                            ChallengeMessage challengeMessage;
+                    ChallengeMessage challengeMessage;
 
-                            if (receiverInfo != null) {
-                                if (((ChallengeMessage) receivedMessage).isBuildConsistency()) {
-                                    challengeMessage = new ChallengeMessage(challengeType, challengeDescription, sender, receiver, pomodoroSession, taskTag, monsterName, taskTitle);
+                    if (receiverInfo != null) {
+                        if (((ChallengeMessage) receivedMessage).isBuildConsistency()) {
+                            challengeMessage = new ChallengeMessage(challengeType, challengeDescription, sender, receiver, pomodoroSession, taskTag, monsterName, taskTitle);
 
-                                } else {
-                                    challengeMessage = new ChallengeMessage(challengeType, challengeDescription, sender, receiver, monsterName, taskTitle);
-
-                                }
-                                receiverInfo.getNetworkUtil().write(challengeMessage);
-
-                            }
+                        } else {
+                            challengeMessage = new ChallengeMessage(challengeType, challengeDescription, sender, receiver, monsterName, taskTitle);
 
                         }
-                        else if(receivedMessage.getMessageType() == MessageType.CHALLENGE_RESPONSE) {
-
-                           String response = ((ChallengeResponse)receivedMessage).getResponseMessage();
-                          ChallengeResponse responseMessage = new ChallengeResponse(sender,receiver,response);
-                            receiverInfo.getNetworkUtil().write(responseMessage);
+                        receiverInfo.getNetworkUtil().write(challengeMessage);
 
                     }
-                        else if(receivedMessage.getMessageType() == MessageType.CHALLENGE_UPDATE){
 
-                            String title = ((ChallengeUpdate)receivedMessage).getTitle();
-                            ChallengeUpdate challengeUpdate = new ChallengeUpdate(sender, receiver, title);
-                            receiverInfo.getNetworkUtil().write(challengeUpdate);
+                } else if (receivedMessage.getMessageType() == MessageType.CHALLENGE_RESPONSE) {
+
+                    String response = ((ChallengeResponse) receivedMessage).getResponseMessage();
+                    ChallengeResponse responseMessage = new ChallengeResponse(sender, receiver, response);
+                    receiverInfo.getNetworkUtil().write(responseMessage);
+
+                } else if (receivedMessage.getMessageType() == MessageType.CHALLENGE_UPDATE) {
+
+                    String title = ((ChallengeUpdate) receivedMessage).getTitle();
+                    ChallengeUpdate challengeUpdate = new ChallengeUpdate(sender, receiver, title);
+                    receiverInfo.getNetworkUtil().write(challengeUpdate);
 
 
-                        }
-                   
-                }catch (Exception e){
-                    System.out.println("Exception occurred in read thread server");
                 }
 
+            } catch (Exception e) {
+                System.out.println("Exception occurred in read thread" +
+                        "");
             }
 
+        }
+
     }
-
-
 }
